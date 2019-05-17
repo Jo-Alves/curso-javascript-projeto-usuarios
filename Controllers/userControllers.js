6f22dee4a630f81d2a88df73d17b6e85c9457038
Jo-Alves <joalves3carvalho@gmail.com>
2019-05-17 20:52:28 -0300

    17/05/2019 B

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -58,7 +58,7 @@ class UserController {
         let isValid = true;
         [...this.formEl.elements].forEach(function (field, index) {
 
-            if (['name', 'email', 'password'].indexOf(field.name > -1 && !field.value)) {
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
                 field.parentElement.classList.add('has-error');
                 isValid = false;
             }
