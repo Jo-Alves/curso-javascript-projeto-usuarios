58343252102102ae54e4f1c276539ac08efb20bd
Jo-Alves <joalves3carvalho@gmail.com>
2019-05-18 17:16:22 -0300

    18/05/2019

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -15,6 +15,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then(content => {
                 values.photo = content;
                 this.addLine(values);
@@ -75,7 +77,7 @@ class UserController {
             }
         });
 
-        if(!isValid){
+        if (!isValid) {
             return false;
         }
         return new User(
@@ -92,9 +94,10 @@ class UserController {
 
     addLine(dataUser) {
 
-        console.log(dataUser);
-        this.TableEl.innerHTML += `   
-            <tr>
+        let tr = document.createElement("tr");
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML += `   
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
@@ -104,10 +107,26 @@ class UserController {
               <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
               <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-            <tr>
             `;
-
+        this.TableEl.appendChild(tr);
+        this.updateCount();
     }
 
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.TableEl.children].forEach(tr => {
+            numberUsers++;
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmin++;
+
+            document.querySelector("#number-users").innerHTML = numberUsers;
+
+            document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+        })
+    }
 
 }
